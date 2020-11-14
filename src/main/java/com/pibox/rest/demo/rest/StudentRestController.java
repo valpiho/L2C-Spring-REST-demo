a6847107a2c9389bb23d030f175be78ad20bdfa6
Valentin Piho <val.piho@gmail.com>
2020-11-14 15:56:03 +0200

    POJO to JSON

---
+++ src/main/java/com/pibox/rest/demo/rest/StudentRestController.java
@@ -0,0 +1,36 @@
+package com.pibox.rest.demo.rest;
+
+import com.pibox.rest.demo.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+
+    private List<Student> studentList;
+
+    @PostConstruct
+    public void loadData() {
+        studentList = new ArrayList<>();
+        studentList.add(new Student("Poornima", "Patel"));
+        studentList.add(new Student("Mario", "Rossi"));
+        studentList.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return studentList;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return studentList.get(studentId);
+    }
+}
